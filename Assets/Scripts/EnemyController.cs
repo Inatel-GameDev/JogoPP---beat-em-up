02cfa59b633a703e8a7bf1c1c65840b2d8a84c1b
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-07-03 02:14:20 -0300

    Movimentação do inimigo completa

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,22 +10,33 @@ public class EnemyController : MonoBehaviour
     
     [SerializeField]private SpriteRenderer oSpriteRenderer;
     private GameObject oPlayer;
+    private Animator oAnimator;
     
     [Header("Enemy Movement")]
     [SerializeField] private float enemySpeed;
     private Vector2 movementDirection;
-    [SerializeField]private float atackDistance;
+
+    [Header("Enemy Attack")]
+    [SerializeField]private float attackCooldown;
+    private float currentCooldown;
+    private bool canAttack;
+    [SerializeField]private float attackDistance;
+    [SerializeField]private int amountOfEnemyAttack;       //quantos ataques tem disponivel
+    private int currentEnemyAttack;        //ataque escolhido
+
 
 
 
     private void Start()
     {
-        oRigidbody2D = GetComponent<Rigidbody2D>();    
+        oRigidbody2D = GetComponent<Rigidbody2D>();  
+        oAnimator = GetComponent<Animator>();  
         oPlayer = FindObjectOfType<PlayerController>().gameObject;             //referencia do jogador
     }
 
     private void Update()
     {
+        CoolDownController();
         Chase();
         EspelharInimigo();
     }
@@ -33,27 +44,70 @@ public class EnemyController : MonoBehaviour
 
     private void EspelharInimigo(){
 
-        if(oRigidbody2D.position.x < 0){
-            oSpriteRenderer.flipX = false;
+        if(oPlayer.transform.position.x > transform.position.x){
+            //oSpriteRenderer.flipX = true;
+            transform.localScale = new Vector3(-1f,1f,1f);
         }
-        else if(oRigidbody2D.position.x > 0){
-            oSpriteRenderer.flipX = true;
+        else{
+            //oSpriteRenderer.flipX = false;
+            transform.localScale = new Vector3(1f,1f,1f);
         }
     }
     
     private void Chase(){
 
         //se estiver distante = segue,  else = ataca
-        if(Vector2.Distance(transform.position,oPlayer.transform.position) > atackDistance){
+        if(Vector2.Distance(transform.position,oPlayer.transform.position) > attackDistance){
 
             //onde inimigo tem que ir = onde ta o player - onde ta o inimigo
             movementDirection = (oPlayer.transform.position - transform.position).normalized;
             oRigidbody2D.velocity = movementDirection * enemySpeed;
 
+            oAnimator.SetTrigger("andando");
         }
+        //para e começa a atacar
         else{
-            oRigidbody2D.velocity = Vector2.zero;      //fica parado
+            oRigidbody2D.velocity = Vector2.zero;      
+            oAnimator.SetTrigger("parado");
+
+            ChooseAttack();
         }
+
+    }
+
+    //escolher ataque de forma aleatorio
+    private void ChooseAttack(){
+        currentEnemyAttack = Random.Range(0,amountOfEnemyAttack);  //escolhe um ataque no intervalo entre a e b, b é exclusivo
+        if(canAttack){
+            Attacking();  //escolhe ataque
+        }
+    }
+
+    //define qual ataque
+    private void Attacking(){
+        
+        switch (currentEnemyAttack)
+        {
+            case 0:
+                oAnimator.SetTrigger("punch");
+                break;
+            case 1:
+                oAnimator.SetTrigger("super-punch");
+                break;            
+        }
+
+        canAttack = false;
+
+    }
+
+    private void CoolDownController(){
+        currentCooldown -= Time.deltaTime;
+        if(currentCooldown <= 0){
+            canAttack = true;
+            currentCooldown = attackCooldown;
+        }
+
+
 
     }
 }
